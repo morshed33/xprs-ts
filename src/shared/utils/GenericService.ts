f521d47c2370703e48e5a995f874e1b7900a3ae1
S M Morshed <ac3m0r@gmail.com>
2025-01-23 11:35:08 +0600

    feat: type safety of GenericService

--- src/shared/utils/GenericService.ts
+++ src/shared/utils/GenericService.ts
@@ -1,10 +1,16 @@
-
 class GenericService<T, CreateInput, WhereInput> {
-    private model: any;
-
-    constructor(model: any) {
-        this.model = model;
+    private model: {
+        create: (args: { data: CreateInput }) => Promise<T>;
+        findUnique: (args: { where: { id: string } }) => Promise<T | null>;
+        findFirst: (args: { where: WhereInput }) => Promise<T | null>;
+        update: (args: { where: { id: string }; data: Partial<T> }) => Promise<T | null>;
+        delete: (args: { where: { id: string } }) => Promise<T | null>;
+        findMany: (args?: { where?: WhereInput }) => Promise<T[]>;
     };
+
+    constructor(model: typeof this.model) {
+        this.model = model;
+    }
 
     async create(data: CreateInput): Promise<T> {
         return await this.model.create({ data });
